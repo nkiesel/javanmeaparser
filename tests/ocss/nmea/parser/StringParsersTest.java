f2eecca893e2fd50affb361f5f906397e621dc9e
Olivier LeDiouris <olivier.lediouris@oracle.com>
2015-04-13 18:03:25 -0700

    Test

--- tests/ocss/nmea/parser/StringParsersTest.java
+++ tests/ocss/nmea/parser/StringParsersTest.java
@@ -23,22 +23,22 @@ public class StringParsersTest
   {
     SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
     sdf.setTimeZone(TimeZone.getTimeZone("Etc/UTC"));
-    
+
     String str = "$IIRMC,220526.00,A,3754.34,N,12223.20,W,3.90,250,,015,E,N*07";
-    
+
     RMC rmc = StringParsers.parseRMC(str);
 //  Date date = rmc.getRmcDate();
     Date time = rmc.getRmcTime();
     GeoPos gp = rmc.getGp();
     double cog = rmc.getCog();
-    double sog = rmc.getSog();      
-    
+    double sog = rmc.getSog();
+
 //    System.out.println("Date:" + date);
 //    System.out.println("Time:" + time + ", [" + sdf.format(time) + "]");
 //    System.out.println("Pos:" + gp.toString() + "(" + gp.lat + ", " + gp.lng + ")");
 //    System.out.println("COG:" + cog);
 //    System.out.println("SOG:" + sog);
-    
+
     assertTrue("Invalid COG",      cog == 250);
     assertTrue("Invalid SOG",      sog == 3.9);
     assertTrue("Invalid Time",     "22:05:26".equals(sdf.format(time)));
@@ -54,7 +54,7 @@ public class StringParsersTest
     String str = "2006-05-05T17:35:48.000Z";
     long ld = StringParsers.durationToDate(str);
 //    System.out.println(str + " => " + new Date(ld) + ", ld:" + ld);
-    assertTrue("Bad duration.", (ld / 1000) == 1146875748L);
+    assertTrue("Bad duration (" + Long.toString(ld / 1000) + ")", (ld / 1000) == 1146875748L);
   }
 
   /**
@@ -67,7 +67,7 @@ public class StringParsersTest
     boolean b = StringParsers.validCheckSum(str);
     assertTrue(str + " is invalid", b);
   }
-  
+
 //  public static void main(String[] args)
 //  {
 //    StringParsersTest spt = new StringParsersTest();
